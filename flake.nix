c59566bc05373d00c5a531ef4f5317e4fd3c94b1
Shaolong Chen <shaolong.chen@outlook.it>
2024-06-30 22:40:57 +0800

    Add nix package

--- flake.nix
+++ flake.nix
@@ -16,13 +16,34 @@
     flake-utils,
     pre-commit-hooks,
     ...
-  } @ inputs:
-    {
-    }
-    // flake-utils.lib.eachDefaultSystem (
+  } @ inputs: let
+    version =
+      if (self ? shortRev)
+      then self.shortRev
+      else "dev";
+  in
+    flake-utils.lib.eachDefaultSystem (
       system: let
         pkgs = nixpkgs.legacyPackages.${system};
-      in {
+      in rec {
+        packages.kvdb = pkgs.buildGoModule {
+          pname = "kvdb";
+          inherit version;
+          src = pkgs.lib.cleanSource self;
+          checkFlags = ["-short"];
+          vendorHash = "sha256-sKmqA7EcreNjgsECB1h63oBN4xfeDODa4x6/G/0HDxU=";
+          subPackages = ["cmd/kvdb-server"];
+          ldflags = ["-s" "-w"];
+          passthru.exePath = "/bin/kvdb-server";
+        };
+        # nix build
+        packages.default = packages.kvdb;
+        apps.kvdb = flake-utils.lib.mkApp {
+          drv = packages.kvdb;
+        };
+        # nix run
+        apps.default = apps.kvdb;
+
         devShells.default = pkgs.mkShell {
           packages = with pkgs; [
             go
@@ -37,6 +58,7 @@
           shellHook = self.checks.${system}.pre-commit-check.shellHook;
         };
 
+        # nix flake check
         checks = {
           pre-commit-check = pre-commit-hooks.lib.${system}.run {
             src = ./.;
@@ -53,6 +75,7 @@
           };
         };
 
+        # nix fmt
         formatter = pkgs.alejandra;
       }
     );
